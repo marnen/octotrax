fcb7ade6f6783875cef05f6e5c74f0ade9c58861
Marnen Laibow-Koser <marnen@marnen.org>
2016-03-08 00:08:56 -0500

    Draw basic commit graph with Graphviz. Styling is off and parent info isn't correct yet (since we need that to be an API call). [#2]

--- octotrax.js
+++ octotrax.js
@@ -1,1 +1,25 @@
-alert('Octotrax is loaded!');
+'use strict';
+
+let commits = $('li.commit');
+commits.attr('data-octotrax-hash', function () {
+  return this.dataset.channel.split(':').slice(-1)[0];
+});
+
+let dot =
+`
+digraph "commit graph" {
+  rankdir = TB;
+`
+
+var childHash;
+commits.toArray().forEach(commit => {
+  let shortHash = commit.dataset.octotraxHash.slice(0, 7);
+  if (childHash) {
+    dot += `  "${childHash}" -> "${shortHash}";\n`;
+  }
+  childHash = shortHash;
+});
+dot += '}';
+
+let svg = $(Viz(dot)).attr({id: 'octotrax-commit-graph'});
+svg.appendTo($('body'));
